1ab6071f9ff43003a92115389ca74ac5debb5b52
Pt1234567 <tripathiprashant45678@gmail.com>
2024-03-28 14:57:37 +0530

    Project Created

---
+++ src/main/java/com/blogApplication/blogapis/BlogApplicationApis.java
@@ -0,0 +1,13 @@
+package com.blogApplication.blogapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApplicationApis {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplicationApis.class, args);
+	}
+
+}
